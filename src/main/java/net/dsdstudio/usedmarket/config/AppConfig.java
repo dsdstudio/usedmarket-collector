11c8435d7010facd2a074ea05331010fe6c9a9f3
보롱킴 <dsdgun@gmail.com>
2016-01-14 23:23:48 +0900

    redis 관련 설정 추가

--- src/main/java/net/dsdstudio/usedmarket/config/AppConfig.java
+++ src/main/java/net/dsdstudio/usedmarket/config/AppConfig.java
@@ -1,7 +1,11 @@
 package net.dsdstudio.usedmarket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,4 +18,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @PropertySource("classpath:/config.properties")
 public class AppConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory f = new JedisConnectionFactory();
+        f.setPort(17300);
+        return f;
+    }
+    @Bean
+    public StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        System.out.println(connectionFactory);
+        return new StringRedisTemplate(connectionFactory);
+    }
 }
