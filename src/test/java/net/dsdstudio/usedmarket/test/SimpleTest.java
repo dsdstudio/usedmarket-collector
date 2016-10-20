0d733f6e54e3a4519fe1006d54e42b117ccca2ba
보롱킴 <dsdgun@gmail.com>
2016-10-21 01:14:08 +0900

    change test for spring boot 1.4.x way

---
+++ src/test/java/net/dsdstudio/usedmarket/test/SimpleTest.java
@@ -0,0 +1,18 @@
+package net.dsdstudio.usedmarket.test;
+
+import net.dsdstudio.usedmarket.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by bhkim on 2016. 1. 14..
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class SimpleTest {
+    @Test
+    public void test() {
+    }
+}
